168e7c7437c17fd0f7f9ec589bd3c0f34d3d78e3
bank <zjubank@gmail.com>
2019-05-30 01:07:43 +0800

    Create xuanke.js

---
+++ xuanke.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Xuanke
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://61.130.6.250:6379/gmlz/xsxk.aspx?xh=<studentID>&lb=A
+// @grant        none
+// ==/UserScript==
+
+(
+    function xuanke() {
+        try{
+            document.getElementById("/*courseID*/").checked = true;
+            //document.getElementById("btnXK").click();
+            __doPostBack('btnXK','');
+        }
+        catch(err){
+            location.reload();
+        }
+        var i = self.setInterval("xuanke()", 100)
+    }
+
+)();
